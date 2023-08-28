b823fc1c51c52277ea376f0b0877ad47746922b3
MariamMo404 <mariammohammed8702@gmail.com>
2023-08-28 20:03:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # write your code here
+favorite_animals=["dog","cat","monkey","rabbit"]
+print(favorite_animals)
+favorite_animals[1]
+favorite_animals.remove("monkey")    
+favorite_animals.append("donkey")
+for animals in favorite_animals:
+    print("i like",animals)
+numbers =[1,2,3,4,5]  
+number_sum =0
+for num in numbers: 
+     number_sum+= num
+
+print(number_sum)
+
+   
